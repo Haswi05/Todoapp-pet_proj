33fb93d07673afa848970758e598b076cbb428b8
Khasein <hasajn98@gmail.com>
2024-01-08 11:59:55 +0300

    Added a new registration for my product

--- todoapp/todolist/models.py
+++ todoapp/todolist/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Todo(models.Model):
     title = models.CharField('Название задачи',max_length=200)
--- todoapp/todolist/urls.py
+++ todoapp/todolist/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('add', views.add, name='add'),
     path('update/<int:todo_id>/', views.update, name='update'),
     path('delete/<int:todo_id>/', views.delete, name='delete'),
+    path('register/',views.register, name='register'),
 ]

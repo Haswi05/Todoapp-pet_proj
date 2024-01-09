845b9a17c9154902dde6c9d84b451ee665a4f6a5
Khasein <hasajn98@gmail.com>
2024-01-09 14:28:18 +0300

    Add new display_view for admin panels

--- todoapp/todolist/admin.py
+++ todoapp/todolist/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from .models import Todo
 # Register your models here.
 
-admin.site.register(Todo)
+@admin.register(Todo)
+class TodoAdminModel(admin.ModelAdmin):
+    list_display = ['title', 'user', 'is_complete']
